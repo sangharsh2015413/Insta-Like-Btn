09d43a8169279fe2a09650ba65d01b46511e3a7d
sangharsh2015413 <95182806+sangharsh2015413@users.noreply.github.com>
2023-10-15 20:11:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var cons=document.querySelector(".container");
+var love=document.querySelector("i");
+cons.addEventListener("dblclick",function(){
+    love.style.transform="translate(-50%,-50%) scale(1)";
+    love.style.color="#ec4033";
+    love.style.opacity=0.8;
+    setTimeout(function(){
+        love.style.opacity=0;
+    },1000);
+    setTimeout(function(){
+        love.style.transform="translate(-50%,-50%) scale(0)";
+    }, 2000);
+})
